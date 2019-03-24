9886c469077469dbdecba8da5f233cf3aa934728
adridevparis <adri@adri.adri>
2019-03-24 12:04:56 +0100

    solve problem 3

--- pb_runner.py
+++ pb_runner.py
@@ -1,11 +1,12 @@
 import time
 from problems.pb1 import Pb1
 from problems.pb2 import Pb2
+from problems.pb3 import Pb3
 
 
 class PbRunner:
     def run(self):
-        pb, answer = self.run_problem(2)
+        pb, answer = self.run_problem(3)
         if pb != None:
             print(pb)
             print("Answer:", answer)
@@ -21,6 +22,9 @@ class PbRunner:
         elif pb_number == 2:
             pb = Pb2()
             answer = pb.get_sum_of_even_terms_less_than(4000000)
+        elif pb_number == 3:
+            pb = Pb3()
+            answer = pb.largest_prime_factor(600851475143)
         return pb, answer
 
 
@@ -31,5 +35,5 @@ if __name__ == '__main__':
     start_time = time.process_time_ns()
     runner.run()
     end_time = time.process_time_ns()
-    print("Execution duration: %g ns" % (end_time - start_time))
+    print("Execution duration: %g ms" % ((end_time - start_time)/1000))
 
---
+++ tests/pb3_test.py
@@ -0,0 +1,12 @@
+import unittest
+from problems.pb3 import Pb3
+
+
+class Pb3Test(unittest.TestCase):
+    def test_given_13195_when_getting_prime_factors_then_largest_is_29(self):
+        pb = Pb3()
+        self.assertEqual(29, pb.largest_prime_factor(13195))
+
+
+if __name__ == '__main__':
+    unittest.main()
